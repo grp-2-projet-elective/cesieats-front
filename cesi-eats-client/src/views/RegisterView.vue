41796ed756f6fc7f34bc93699aad7e12601aec1a
Vincentkayak <vincent.laclide@viacesi.fr>
2022-06-25 11:36:06 +0200

    registerview

---
+++ cesi-eats-client/src/views/RegisterView.vue
@@ -0,0 +1,16 @@
+<template>
+    <RegisterComponent/>
+</template>
+
+<script lang="typescript">
+import Vue from 'vue'
+import RegisterComponent from '@/components/RegisterComponent.vue'
+
+export default Vue.extend({
+  name: 'RegisterView',
+
+  components: {
+    RegisterComponent
+  }
+})
+</script>
