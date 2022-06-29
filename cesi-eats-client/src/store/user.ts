26209381fb25b3e0260556499c5779912410e694
Félicien Fouillet <50355349+felicienfouillet@users.noreply.github.com>
2022-06-29 23:25:17 +0200

    (evo): setup token registration (#53)

--- cesi-eats-client/src/store/user.ts
+++ cesi-eats-client/src/store/user.ts
@@ -1,7 +1,10 @@
 const user = window.localStorage.getItem('user')
+const tokens = window.localStorage.getItem('tokens')
 
 const state = {
-  user: user ? JSON.parse(user) : []
+  user: user ? JSON.parse(user) : [],
+  accessToken: tokens ? JSON.parse(tokens).accessToken : null,
+  refreshToken: tokens ? JSON.parse(tokens).refreshToken : null
 }
 
 const mutations = {
@@ -9,8 +12,16 @@ const mutations = {
     state.user = user
     this.commit('saveUser')
   },
+  defineTokens (state, tokens) {
+    state.accessToken = tokens.accessToken
+    state.refreshToken = tokens.refreshToken
+    this.commit('saveTokens')
+  },
   saveUser (state) {
     window.localStorage.setItem('user', JSON.stringify(state.user))
+  },
+  saveTokens (state) {
+    window.localStorage.setItem('tokens', JSON.stringify({ accessToken: state.accessToken, refreshToken: state.refreshToken }))
   }
 }
 
